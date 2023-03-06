488c15263a0bdcf9b13b2a3adf4208d0927560d3
goshva <sha.egor@gmail.com>
2023-03-06 14:01:07 +0300

    add infinity slider

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,10 @@
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
 
-// icon zooming
+// icon zooming'
+
+let n = 0 ;
+
 var modal = document.getElementById('myModal')
 var imgs = document.getElementsByClassName('example-image');
 var modalImg = document.getElementById('img01')
@@ -20,7 +23,7 @@ for(i = 0; i < imgs.length; i++){
 
 function openImg(pic){
     //modal.style.display='grid';
-    modal.style.display='inline-block';
+    modal.style.display='flex';
     modalImg.src = pic.src;
 }
 
@@ -29,12 +32,22 @@ function close(){
 }
 
 
-function lineAddImage(){
-    modal.appendChild(imgs[0])
+function lineAddImage(n){
+    addImg = new Image();
+    addImg.src = imgs[n]?.src;
+    if (!!imgs[n]) {
+        n =0;
+        addImg.classList.add("modal-content");
+        modal.appendChild(addImg)
+     }
+     else {
+        close()
+     }
 };
 function myScript (e){
     let scroll = modal.scrollLeft;
-    if(scroll >= (modal.scrollWidth - modal.clientWidth)){ lineAddImage()};
+    if(scroll >= (modal.scrollWidth - modal.clientWidth)){ n++ ;lineAddImage(n%imgs.length)};
+    //console.log(n)
 
 };
 modal.addEventListener("scroll", (e) => {myScript(e)});
